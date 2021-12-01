28551aa11b923bfc46099ba1e50f2efd31835e8b
needh <needh@DESKTOP-JPDVAQT>
2021-11-30 23:01:31 -0300

    Se modifican nombres de PC
    Se modificaron los nombres de PC para indicar nombres de integrantes

--- src/DataBase/Conexion.java
+++ src/DataBase/Conexion.java
@@ -7,7 +7,7 @@ import controlador.Registro;
 
 /**
  *
- * @author Derek Needham <deneedham@duocuc.cl>
+ * @author Nelly Becerra - Derek Needham
  * @version 23-11-2021
  */
 public class Conexion {
